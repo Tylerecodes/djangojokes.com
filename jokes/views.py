f8c50b626030c8b119e0da4c803a13812ce75b3f
Tyler Estes <gregorytestes@outlook.com>
2022-07-06 08:32:45 -0500

    Added forms for creating new and updating jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from .models import Joke
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
 
 class JokeListView(ListView):
     model = Joke

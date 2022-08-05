061af4b30ae1d22f9254f09657b25f57f0e16254
Tyler Estes <gregorytestes@outlook.com>
2022-08-04 21:53:23 -0500

    Impleted ModelForm for joke adding and updating

--- jokes/views.py
+++ jokes/views.py
@@ -3,21 +3,27 @@ from django.views.generic import (
 )
 from .models import Joke
 from django.urls import reverse_lazy
+from .forms import JokeForm
+
 
 class JokeUpdateView(UpdateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
+
 
 class JokeCreateView(CreateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
+
 
 class JokeDeleteView(DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
 
+
 class JokeListView(ListView):
     model = Joke
+
 
 class JokeDetailView(DetailView):
     model = Joke

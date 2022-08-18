66bd232693689177cd3623b9e203235dfe2f8343
Tyler Estes <gregorytestes@outlook.com>
2022-08-18 08:38:24 -0400

    Set up django auth and user registration

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # User Management
+    path('account/', include('allauth.urls')),
+
+    # Admin
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+
+    # Local Apps
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),

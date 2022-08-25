ea9f5dd2dcf76c2b23a3c90df4ac3c99e2729ec0
Tyler Estes <gregorytestes@outlook.com>
2022-08-24 22:20:05 -0500

    Added SuccessMessage to joke upd and create view

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import (
     CreateView, DeleteView, UpdateView, DetailView, ListView
 )
@@ -7,18 +8,20 @@ from django.urls import reverse_lazy
 from .forms import JokeForm
 
 
-class JokeUpdateView(UserPassesTestMixin, UpdateView):
+class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke Updated'
 
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
 
 
-class JokeCreateView(LoginRequiredMixin, CreateView):
+class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke Created'
 
     def form_valid(self, form):
         form.instance.user = self.request.user

3b5fe24008266b4c2d1f2ea3d2370f1a09e1cc18
Tyler Estes <gregorytestes@outlook.com>
2022-08-24 22:01:48 -0500

    Restricted Joke create and update. Added Messages

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
+from django.contrib import messages
 from pipes import Template
 from django.views.generic import TemplateView
 
+
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about-us.html'

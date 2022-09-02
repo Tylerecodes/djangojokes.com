02b4ed205e893b21b40020c072bf77e0800e4084
Tyler Estes <gregorytestes@outlook.com>
2022-09-01 20:11:48 -0500

    Implemented search function for jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,5 @@
 import json
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import JsonResponse
@@ -101,6 +102,12 @@ class JokeListView(ListView):
     def get_queryset(self):
         ordering = self.get_ordering()
         qs = Joke.objects.all()
+
+        if 'q' in self.request.GET:  # Filter by search query
+            q = self.request.GET.get('q')
+            qs = qs.filter(
+                Q(question__icontains=q) | Q(answer__icontains=q)
+            )
 
         if 'slug' in self.kwargs:  # Filter by category or tag
             slug = self.kwargs['slug']

afba4bcbc8f6823420b792df3c2a51a681ba21a2
Tyler Estes <gregorytestes@outlook.com>
2022-09-05 10:14:02 -0500

    Updated admin titles

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+admin.site.index_title = 'Home'
+admin.site.site_title = 'Django Jokes Admin'
+admin.site.site_header = 'Django Jokes Admin'
 # Register your models here.

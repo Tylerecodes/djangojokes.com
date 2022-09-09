ef9055c49ce492fc8aa7be36d2cacc8a788ee9fb
Tyler Estes <gregorytestes@outlook.com>
2022-09-08 20:26:12 -0500

    Django Admin updates-Final Commit

--- jokes/admin.py
+++ jokes/admin.py
@@ -24,9 +24,16 @@ class JokeAdmin(DjangoJokesAdmin):
     list_filter = ['updated', 'category', 'tags']
     date_hierarchy = 'updated'
 
+    # Form Attributes
+    autocomplete_fields = ['tags', 'user']
+    radio_fields = {'category': admin.HORIZONTAL}
+
+    def vote_summary(self, obj):
+        return f'{obj.num_votes} votes. Rating: {obj.rating}.'
+
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
-            return ('slug', 'created', 'updated')
+            return ('slug', 'created', 'updated', 'vote_summary')
 
         return ()
 
@@ -46,6 +53,7 @@ class JokeVoteAdmin(DjangoJokesAdmin):
 class TagAdmin(DjangoJokesAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
+    search_fields = ['tag']
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
